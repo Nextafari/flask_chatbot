3e996ed1090a0d9be79a8b7d18339a4ddfa5d893
Nextafari <chinedue16@gmail.com>
2020-08-02 16:14:47 +0100

    Feat

--- wsgi.py
+++ wsgi.py
@@ -3,4 +3,4 @@ from application import application
 if __name__ == "__main__":
     application.run(debug=True,host='0.0.0.0')
 
-web: gunicorn -w 4 -b 0.0.0.0 application:app
+#web: gunicorn -w 4 -b 0.0.0.0 application:app
